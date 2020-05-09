f8fc9b68042d69c10f7cdd5b29a9522652734b0d
enricotj <taiphoon007@gmail.com>
2020-05-09 14:28:20 -0400

    Fix webpack config error.

--- webpack.config.js
+++ webpack.config.js
@@ -36,10 +36,11 @@ module.exports = {
       }
 
     ],
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html'
-      })
-    ]
-  }
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 };
